639fb378d52cf3ed56042f069362950a63eea9fe
Developer-Elisha <developer.elisha@gmail.com>
2025-02-18 14:14:51 +0500

    fix server routes on '/'

--- server/index.js
+++ server/index.js
@@ -11,12 +11,15 @@ connectDB();
 app.use(cors());
 app.use(express.json());
 
-app.use('/api/auth', require('./routes/auth'));
+app.use('/', require('./routes/auth'));
 app.use(cors({ origin: 'http://localhost:5173', credentials: true }));
 
+
+
 app.get('/', (req, res) => {
-    res.send('API is running');
+    res.send('Welcome to the authentication API');
 });
+
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
