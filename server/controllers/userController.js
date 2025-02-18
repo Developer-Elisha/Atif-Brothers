43d737faed6c2e6b40cc6c5af5d7eb28739e09b9
Developer-Elisha <developer.elisha@gmail.com>
2025-02-18 16:50:11 +0500

    again setup server

--- server/controllers/userController.js
+++
@@ -1,9 +0,0 @@
-const User = require('../models/userModel');
-
-// Delete All Users
-const deleteAllUsers = async (req, res) => {
-  await User.deleteMany();
-  res.status(200).json({ message: 'All users deleted' });
-};
-
-module.exports = { deleteAllUsers };
--- server/routes/userRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const { deleteAllUsers } = require('../controllers/userController');
-const protect = require('../middlewares/authMiddleware');
-const router = express.Router();
-
-router.delete('/delete-all', protect, deleteAllUsers);
-
-module.exports = router;
