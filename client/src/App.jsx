d2384cf83d562a981e1cb78222d75d7831e938cd
Developer-Elisha <developer.elisha@gmail.com>
2025-03-18 13:02:15 +0500

    tagamount, Order Status, Cancel, Given, Dasti/Return Cards and other changes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -12,7 +12,7 @@ import ExpenseDashboard from "./components/Expense/ExpenseDashboard";
 import DealerDashboard from "./components/KapraDealer/DealarDashboard";
 import DetailsDashboard from "./components/Details/DetailsDashboard";
 import BankDashboard from "./components/Bank/BankDashboard";
-// import StitchingDashboard from "./components/Stitching/StitchingDashboard";
+import StitchingDashboard from "./components/Stitching/StitchingDashboard";
 import DastiDashboard from "./components/Dasti/DastiDashboard";
 import ReturnDashboard from "./components/Return/ReturnDashboard";
 import TailorDashboard from "./components/Tailor/TailorDashboard";
@@ -22,181 +22,93 @@ import ShowroomDailyExpense from "./components/ShowroomDailyExpense/ShowroomDail
 import ShowroomStaffDashboard from "./components/ShowroomStaff/ShowroomStaffDashboard";
 import ShowroomCustomerDashboard from "./components/ShowroomCustomers/ShowroomCustomerDashboard";
 
-const PrivateRoute = ({ element, allowedRoles }) => {
+const PrivateRoute = ({ element }) => {
   const token = localStorage.getItem("token");
-  const userRole = localStorage.getItem("userRole");
-
-  if (!token) {
-    return <Navigate to="/login" replace />;
-  }
-
-  if (allowedRoles && !allowedRoles.includes(userRole)) {
-    return <Navigate to={userRole === 'admin' ? '/' : '/showroom'} replace />;
-  }
-
-  return element;
+  return token ? element : <Navigate to="/login" replace />;
 };
 
 function App() {
   return (
     <Router>
       <Routes>
-        {/* Showroom Routes - User Only */}
         <Route
           path="/showroom"
-          element={<PrivateRoute 
-            element={<ShowroomLayout><ShowroomDailyExpense /></ShowroomLayout>}
-            allowedRoles={['user']}
-          />}
-        />
-
+          element={<ShowroomLayout><ShowroomDailyExpense /></ShowroomLayout>}
+        /> 
+        
         <Route
           path="/showroomcustomer"
-          element={<PrivateRoute 
-            element={<ShowroomLayout><ShowroomCustomerDashboard /></ShowroomLayout>}
-            allowedRoles={['user']}
-          />}
-        />
-
+          element={<ShowroomLayout><ShowroomCustomerDashboard /></ShowroomLayout>}
+        /> 
+        
         <Route
           path="/showroomstaff"
-          element={<PrivateRoute 
-            element={<ShowroomLayout><ShowroomStaffDashboard /></ShowroomLayout>}
-            allowedRoles={['user']}
-          />}
+          element={<ShowroomLayout><ShowroomStaffDashboard /></ShowroomLayout>}
         />
-
-        {/* Admin Routes */}
         <Route
           path="/"
-          element={<PrivateRoute 
-            element={<Layout><DailyExpense /></Layout>}
-            allowedRoles={['admin']}
-          />}
-        />
-
+          element={<Layout><DailyExpense /></Layout>}
+        /> 
         <Route
           path="/staff"
-          element={<PrivateRoute 
-            element={<Layout><StaffDashboard /></Layout>}
-            allowedRoles={['admin']}
-          />}
+          element={<Layout><StaffDashboard /></Layout>}
         />
-
         <Route
           path="/dashboard"
-          element={<PrivateRoute 
-            element={<Layout><KarigerDashboard /></Layout>}
-            allowedRoles={['admin']}
-          />}
+          element={<Layout><KarigerDashboard /></Layout>}
         />
-
         <Route
           path="/kapra"
-          element={<PrivateRoute 
-            element={<Layout><KapraDashboard /></Layout>}
-            allowedRoles={['admin']}
-          />}
+          element={<Layout><KapraDashboard /></Layout>}
         />
-
         <Route
           path="/customer"
-          element={<PrivateRoute 
-            element={<Layout><CustomerDashboard /></Layout>}
-            allowedRoles={['admin']}
-          />}
-        />
-
+          element={<Layout><CustomerDashboard /></Layout>}
+        /> 
         <Route
           path="/expense"
-          element={<PrivateRoute 
-            element={<Layout><ExpenseDashboard /></Layout>}
-            allowedRoles={['admin']}
-          />}
-        />
-
+          element={<Layout><ExpenseDashboard /></Layout>}
+        /> 
         <Route
           path="/dealer"
-          element={<PrivateRoute 
-            element={<Layout><DealerDashboard /></Layout>}
-            allowedRoles={['admin']}
-          />}
-        />
-
+          element={<Layout><DealerDashboard /></Layout>}
+        /> 
         <Route
           path="/details"
-          element={<PrivateRoute 
-            element={<Layout><DetailsDashboard /></Layout>}
-            allowedRoles={['admin']}
-          />}
-        />
-
+          element={<Layout><DetailsDashboard /></Layout>}
+        /> 
         <Route
           path="/bank"
-          element={<PrivateRoute 
-            element={<Layout><BankDashboard /></Layout>}
-            allowedRoles={['admin']}
-          />}
-        />
-
-        {/* <Route
+          element={<Layout><BankDashboard /></Layout>}
+        /> 
+        <Route
           path="/stitching"
-          element={<PrivateRoute 
-            element={<Layout><StitchingDashboard /></Layout>}
-            allowedRoles={['admin']}
-          />}
-        /> */}
-
+          element={<Layout><StitchingDashboard /></Layout>}
+        /> 
         <Route
           path="/dasti"
-          element={<PrivateRoute 
-            element={<Layout><DastiDashboard /></Layout>}
-            allowedRoles={['admin']}
-          />}
-        />
-
+          element={<Layout><DastiDashboard /></Layout>}
+        /> 
         <Route
           path="/return"
-          element={<PrivateRoute 
-            element={<Layout><ReturnDashboard /></Layout>}
-            allowedRoles={['admin']}
-          />}
-        />
-
+          element={<Layout><ReturnDashboard /></Layout>}
+        /> 
         <Route
           path="/tailor"
-          element={<PrivateRoute 
-            element={<Layout><TailorDashboard /></Layout>}
-            allowedRoles={['admin']}
-          />}
-        />
-
+          element={<Layout><TailorDashboard /></Layout>}
+        /> 
         <Route
           path="/miscellaneous"
-          element={<PrivateRoute 
-            element={<Layout><MiscellaneousDashboard /></Layout>}
-            allowedRoles={['admin']}
-          />}
-        />
-
+          element={<Layout><MiscellaneousDashboard /></Layout>}
+        /> 
         <Route
           path="/brother"
-          element={<PrivateRoute 
-            element={<Layout><DilNawazDashboard /></Layout>}
-            allowedRoles={['admin']}
-          />}
+          element={<Layout><DilNawazDashboard /></Layout>}
         />
 
+        
         <Route path="/login" element={<Login />} />
-        <Route 
-          path="*" 
-          element={
-            <Navigate 
-              to={localStorage.getItem("userRole") === 'admin' ? '/' : '/showroom'} 
-              replace 
-            />
-          } 
-        />
+        <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
     </Router>
   );
